00e3005e38550f0ceed9e658d6ea0442c49a59f4
Pranjal Chavda <pranjalchavda16@gmail.com>
2026-08-11 18:54:24 +0530

    clear task functionality

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 let btn=document.querySelector('.add-icon');
 let input=document.querySelector('#add-input');
 let tasksContainer=document.querySelector('.tasks');
+let deleteBtn=document.querySelector('.delete-icon');
 
 btn.addEventListener('click',function(){
     let inputValue=input.value;
 
-    if(inputValue.trim() === ''){
+    if(inputValue.trim()===''){
         return;
     }
 
-    tasksContainer.innerHTML += `<div class="task">${inputValue}</div>`;
+    tasksContainer.innerHTML+=`<div class="task">${inputValue}</div>`;
+    input.value='';
+});
+
+deleteBtn.addEventListener('click',function(){
+    tasksContainer.innerHTML='';
 });
